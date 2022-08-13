4201c223acf628882149a95c7fb95db07147e405
tanviralam1998 <tanvir.ctg1998@gmail.com>
2022-08-13 18:49:42 +0600

    new arrivel section done

---
+++ js/script.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        ltr:true,
+        loop:true,
+        autoplay:true,
+        autoplayTimeout:2000,
+        autoplayHoverPause:true,
+        margin:10,
+        items:4,
+        nav:true,
+        dots:false,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:true
+            },
+            600:{
+                items:3,
+                nav:false
+            },
+            1000:{
+                items:4,
+                nav:true,
+                loop:false
+            }
+        }
+    
+    })
+    
+});
